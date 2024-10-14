1badbefd8dbdb904652420f1354d7b09c41da308
farhanisty <farhannivtasija@gmail.com>
2024-10-14 23:20:45 +0700

    feat(tailwind): create custom color pallete

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,15 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'custom': {
+				'dark': '#000',
+				'darkblue': '#14213D',
+				'yellow': '#FCA311',
+				'gray': '#E5E5E5',
+				'light': '#FFFFFF'
+			}
+		},
 	},
 	plugins: [],
 }
